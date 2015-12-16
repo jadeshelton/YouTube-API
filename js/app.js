03758cb284c34aed53d67a6fdeecd97434379671
jadeshelton <jnshelton257@yahoo.com>
2015-12-15 19:07:28 -0800

    remove previous results js

--- js/app.js
+++ js/app.js
@@ -1,38 +1,10 @@
-/*$(function(){
 
-	$("#search-term").submit(function(event) {
-		event.preventDefault();
-		var searchTerm = $("#query").val();
-		getRequest(searchTerm);
-		});
-});
-
-function getRequest(searchTerm){
-		var params = {
-			q: searchTerm,
-			part: 'snippet',
-			key: 'AIzaSyCzVPRJ1vYhkCQim0a0KmyFY9pKtBINDgw'
-		};
-		url = 'https://www.googleapis.com/youtube/v3/search';
-		$.getJSON(url, params, function(data) {
-			showResults(data.items);
-	});
-}
-
-
-function showResults(results){
-	$.each(results, function(index, item){
-		$('#search-results').append('<a href="https://www.youtube.com/watch?v=' + item.id.videoId + '"><img class="thumbnail" style="padding: 1em" height="180px" width="321px" title="' + item.snippet.title + '" src="' + item.snippet.thumbnails.medium.url + '""></a>');
-		console.log(item);
-	});
-}
-
-*/
 
 $(function(){
 
 	$("#search-term").submit(function(event) {
 		event.preventDefault();
+		$('#search-results').html('');
 		var searchTerm = $("#query").val();
 		getRequest(searchTerm);
 		});
